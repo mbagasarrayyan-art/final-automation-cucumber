15aba536ee248c9de6aab5504b38320671e0881f
Bagas Arrayyan <mbagasarrayyan@gmail.com>
2026-01-09 13:32:56 +0700

    Fix runners,gradle tasks, and headless hooks for CI

--- src/test/java/com/bagas/finalproject/web/runner/WebTestRunner.java
+++ src/test/java/com/bagas/finalproject/web/runner/WebTestRunner.java
@@ -5,17 +5,13 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.*;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/web")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.bagas.finalproject.web.steps")
 @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@web")
-@ConfigurationParameter(
-        key = PLUGIN_PROPERTY_NAME,
-        value = "pretty," +
-                "html:build/reports/cucumber/web/cucumber.html," +
-                "json:build/reports/cucumber/web/cucumber.json"
-)
-public class WebTestRunner { }
+public class WebTestRunner {
+}
--- src/test/java/com/bagas/finalproject/web/steps/Hooks.java
+++ src/test/java/com/bagas/finalproject/web/steps/Hooks.java
@@ -5,19 +5,31 @@ import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
+
     public static WebDriver driver;
 
     @Before("@web")
-    public void beforeWeb() {
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Aman untuk CI (GitHub Actions) + tetap bisa jalan di lokal
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
     }
 
     @After("@web")
-    public void afterWeb() {
-        if (driver != null) driver.quit();
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
