daf30f84fd4dd3e6dae83ad763a7caf483e6b0ef
Rizqi Ahmad Fauzan <sipena.id@gmail.com>
2026-01-23 14:18:19 +0700

    add cucumber report

--- src/test/java/com/bagas/finalproject/web/runner/WebTestRunner.java
+++ src/test/java/com/bagas/finalproject/web/runner/WebTestRunner.java
@@ -7,11 +7,13 @@ import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/web")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.bagas.finalproject.web.steps")
 @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@web")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:build/reports/cucumber/web-report.json,html:build/reports/cucumber/web-report.html")
 public class WebTestRunner {
 }
